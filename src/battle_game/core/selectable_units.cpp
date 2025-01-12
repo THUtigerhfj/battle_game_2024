848d56d30e913f9951c6e0e9227b5404011f96d0
THUtigerhfj <hfj24@mails.tsinghua.edu.cn>
2025-01-12 10:53:02 +0800

    enable self-damage due to collision with obstacles

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -21,6 +21,7 @@ void GameCore::GeneratePrimaryUnitList() {
   /*
    * TODO: Add Your Unit Here!
    * */
+  ADD_SELECTABLE_UNIT(unit::Tank_hfj)
   ADD_SELECTABLE_UNIT(unit::Tank);
 
   unit.reset();
